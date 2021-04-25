3fed2207636348dc4096f09cdb7741294510ce77
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-04-25 16:06:13 -0500

    file init and schema build

---
+++ schema.sql
@@ -0,0 +1,29 @@
+  
+DROP DATABASE IF EXISTS employee_db;
+
+CREATE DATABASE employee_db;
+
+USE employeeTrackerDB;
+
+CREATE TABLE employees (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    firstName VARCHAR(30),
+    lastName VARCHAR(30),
+    role_id INTEGER,
+    manager_id INTEGER,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE departments (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+    id INTEGER NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL(10,2),
+    department_id INTEGER,
+    PRIMARY KEY (id)
+);
