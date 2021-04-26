d1c1e63cf199018500de913636c89c9be9534346
Zane-Gilfillan <zane.gilfillan@gmail.com>
2021-04-26 18:05:34 -0500

    working on db functionality

--- db.js
+++ db.js
@@ -23,3 +23,52 @@ db.connect(err => {
     app.init();
 });
 
+let showAll = (table_name,callback) => {
+    let query = "";
+    if (table_name === "employees") {
+
+        // show all employees 
+
+        query = `SELECT emp1.firstName AS 'First Name', emp1.lastName AS 'Last Name', title AS 'Title', name AS 'Department', salary AS 'Salary', GROUP_CONCAT(DISTINCT emp2.firstName,' ', emp2.lastName) AS 'Manager'
+        FROM employees emp1
+        JOIN roles ON emp1.role_id = roles.id
+        JOIN departments ON roles.department_id = departments.id
+        LEFT JOIN employees emp2 ON emp1.manager_id = emp2.id
+        GROUP BY emp1.id
+        ORDER BY emp1.lastName ASC`;
+    } else if (table_name === "roles") {
+
+        // show all rolels   
+
+        query = `SELECT title AS 'Position', name AS 'Department', salary AS 'Salary', COUNT(employees.role_id) AS 'Total Employees'
+        FROM roles
+        LEFT OUTER JOIN departments ON roles.department_id = departments.id
+        LEFT OUTER JOIN employees ON employees.role_id = roles.id
+        GROUP BY roles.id
+        ORDER BY title ASC`;
+    } else if (table_name === "departments") {
+
+        // show all departments
+        
+        query = `SELECT name AS 'Department', COUNT(roles.department_id) AS 'Total Roles'
+        FROM departments
+        LEFT OUTER JOIN roles ON roles.department_id = departments.id
+        GROUP BY departments.id
+        ORDER BY name ASC`;
+    }
+
+    db.query(query,table_name,(err,res) => {
+        if (err) throw err;
+        console.log('\n');
+        console.table(res);
+        callback();
+    });
+}
+
+let createRow = (data,table_name,callback) => {
+    db.query(`INSERT INTO ${table_name} SET ?`,[data],function(err,res) {
+        if (err) throw err;
+        console.log("\nSuccess! Added to "+table_name+".\n");
+        callback();
+    });
+}
